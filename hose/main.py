406012393b933656c003341289b46239f04773a0
Lane Kolbly <lane@rscheme.org>
2012-12-26 19:39:09 -0600

    Implemented basic sockets between hose and lawn.

--- hose/main.py
+++ hose/main.py
@@ -1,1 +1,24 @@
 import paramiko #ssh library
+import socket
+import json
+import struct
+
+class LawnConnection:
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+    def sendPacket(self, packet):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.hostname, 4321))
+        cmd = json.dumps(packet)
+        s.send(struct.pack("I", len(cmd)))
+        s.send(cmd)
+
+        # Recieve the return value
+        l = struct.unpack("I", s.recv(4))[0]
+        ret = s.recv(l)
+        print ret
+        return ret
+
+l = LawnConnection("127.0.0.1")
+l.sendPacket({"cmd": "uptime"})
--- lawn/client.py
+++ lawn/client.py
@@ -1,1 +1,31 @@
 print "I am the client, my job is to connect to the hose"
+
+import paramiko
+import socket
+import threading
+import json
+import struct
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("localhost", 4321))
+s.listen(10)
+
+while True:
+    s2, addr = s.accept()
+
+    # First four bytes are the packet length
+    l = struct.unpack("I", s2.recv(4))[0]
+    cmd = json.loads(s2.recv(l))
+    print cmd
+
+    import subprocess
+
+    args = cmd.get("args", [])
+    proc = subprocess.Popen([cmd["cmd"]]+args, stdout=subprocess.PIPE)
+    ret = proc.communicate()[0]
+
+    ret_s = json.dumps({"retval": ret})
+    s2.send(struct.pack("I", len(ret_s)))
+    s2.send(ret_s)
+    s2.close()
